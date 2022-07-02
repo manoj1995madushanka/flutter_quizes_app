bde33a7a829f0364a9e4bfeaaca7b7c4b1e5fcf2
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-07-03 00:45:41 +0530

    final vs const

--- lib/question.dart
+++ lib/question.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-  String questionText;
+  final String questionText;
 
   // this is called position arguments the first constructor argument always maps to
   // questionText property
