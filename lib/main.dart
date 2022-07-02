9db3a510cc6b503c7400b6984a662b07bcddcc9d
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-07-02 20:08:03 +0530

    answer question functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,52 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  var questionIndex = 0;
+
+  var questions = [
+    'what\'s your favourite color?',
+    'what\'s your favourite animal?'
+  ];
+
+  void answerQuestion() {
+    questionIndex += 1;
+    print(questionIndex);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text("MyApp"),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("AppBar"),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            ElevatedButton(
+              child: Text("Answer 1"),
+              onPressed: answerQuestion,
+              // here only added method name this will pointer to actual method note the method return value
+            ),
+            ElevatedButton(
+              onPressed: () => {print("answer 2 chosen")},
+              child: Text("Answer 2"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print("answer 3 chosen");
+              },
+              child: Text("Answer 3"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
